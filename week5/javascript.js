b635255a92f74addd1e98e7cb878dc9101e8acd3
Matthew Aaron Pinlac <119918170+mapinlac@users.noreply.github.com>
2023-01-24 22:40:47 +0800

    JS Loop For

--- week5/javascript.js
+++ week5/javascript.js
@@ -2,30 +2,10 @@ function displayDate() {
     document.getElementById("time").innerHTML = Date();
 }
 
-function displayDay(){
-    let day;
-    switch (new Date().getDay()) {
-      case 0:
-        day = "Sunday";
-        break;
-      case 1:
-        day = "Monday";
-        break;
-      case 2:
-        day = "Tuesday";
-        break;
-      case 3:
-        day = "Wednesday";
-        break;
-      case 4:
-        day = "Thursday";
-        break;
-      case 5:
-        day = "Friday";
-        break;
-      case  6:
-        day = "Saturday";
-        document.getElementById("day").innerHTML = "Today is " + day;
+
+const hobbies = ["Soccer", "Computer Games", "Reading"];
+let text = "";
+for (let i = 0; i < hobbies.length; i++) {
+  text += hobbies[i] + "<br>";
 }
-document.getElementById("demo").innerHTML = 100 + 50 * 3;
-}  
+document.getElementById("demo").innerHTML = text;
