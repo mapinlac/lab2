805e1e922011d93ccc67c6400ea6f9378ae844d9
Matthew Aaron Pinlac <119918170+mapinlac@users.noreply.github.com>
2023-01-24 22:52:44 +0800

    JavaScript Objects

--- week5/javascript.js
+++ week5/javascript.js
@@ -8,4 +8,9 @@ for (let i = 0; i < hobbies.length; i++) {
   text += hobbies[i] + "<br>";
 }
 document.getElementById("hobbies").innerHTML = text;
+
 document.getElementById("random").innerHTML = Math.random();
+
+const Apex = { title:"APEX", type:"FPS Battle Royale", team:"3 people"};
+
+document.getElementById("apex").innerHTML = "The game is called " + Apex.title + "it is a " + Apex.type + "and can be played with " + Apex.team;
