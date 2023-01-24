518ed11d5f2f542153cc1ce11c3b4bda31b0f1b2
Matthew Aaron Pinlac <119918170+mapinlac@users.noreply.github.com>
2023-01-24 22:46:59 +0800

    JavaScript Random

--- week5/javascript.js
+++ week5/javascript.js
@@ -2,10 +2,10 @@ function displayDate() {
     document.getElementById("time").innerHTML = Date();
 }
 
-
 const hobbies = ["Soccer", "Computer Games", "Reading"];
 let text = "";
 for (let i = 0; i < hobbies.length; i++) {
   text += hobbies[i] + "<br>";
 }
-document.getElementById("demo").innerHTML = text;
+document.getElementById("hobbies").innerHTML = text;
+document.getElementById("random").innerHTML = Math.random();
