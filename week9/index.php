a77671dc64a6cc3954e4d8252a9bfa54a6ca0639
Matthew Aaron Pinlac <119918170+mapinlac@users.noreply.github.com>
2023-01-31 15:13:29 +0800

    update

---
+++ week9/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=neon|outline|emboss|shadow-multiple">
+  <link rel="icon" href="https://pbs.twimg.com/media/FPOa6WpWUAQE2QH.jpg">
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <title>About Matz</title>
+
+</head>
+<body>
+
+<div class="sticky"><h1><center>Introductions!</center></h1></body></html>
+</div>
+
+<section><center>
+<h2>FULL NAME:</h2>
+<p class="textshadow">MATTHEW AARON A. PINLAC
+  <i class="material-icons">cloud</i>
+</p>
+</center></section>
+
+<section><center>
+<h3>Nickname:</h3>
+<p class= "linethrough">AMATZ</p>
+<p class="wavyunderline">Matz</p>
+</center></section>
+ 
+<section><center>
+<h4>Age:</h4>
+<p>19 Years old</p>
+</center></section>
+
+<section><center>
+<h5>Senior High School Graduated From:</h5>
+<p>Lyceum of the Philippines Cavite, General Trias Cavite</p>
+</center></section>
+
+<section><center>
+<p>HTML Tutorial</p>
+<p><b><a class="one" href="https://www.w3schools.com">Visit W3Schools</a></b></p>
+<a href="..\.."><p>Back to REAL</p></a>
+</center>
+</section>
+
+<section><center>
+<button onclick="displayDate()">Time and Date</button>
+<p id="time"></p>
+</center>
+</section>
+
+<section><center>
+<p>My hobbies</p>
+<p id="hobbies"></p>
+</center>
+</section>
+
+<section><center>
+<p>Here is a random number between 0 (included) and 1 (excluded)</p>
+<p id="random"></p>
+</center>
+</section>
+
+<section><center>
+<p>APEX LEGENDS</p>
+<p id="apex"></p>
+</center>
+</section>
+
+<section><center>
+<p>Functionsss 5 x 6</p>
+<p id="function"></p>
+</center>
+</section>
+
+<section><center>
+  <p>Catching Error</p>
+  <p id="error"></p>
+  </center>
+  </section>
+</body>
+<script src="javascript.js"></script>
+</html>
