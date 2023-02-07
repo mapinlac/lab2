53b21458335187957703d9b723b283c41ce8f542
Matthew Aaron Pinlac <119918170+mapinlac@users.noreply.github.com>
2023-02-07 15:00:11 +0800

    guests edit

--- week9/guests.php
+++ week9/guests.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["Name"]. " - Email " . $row["Email"]. "<br>";
   }
 } else {
   echo "0 results";
